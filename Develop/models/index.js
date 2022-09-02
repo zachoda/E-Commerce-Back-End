b67b9b4c41dc519576577318db7ccbd40220aba4
Zach Oda <zjodachow@gmail.com>
2022-09-02 12:04:30 -0400

    associations & models created

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -3,14 +3,30 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsToMany } = require('./Product');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: "category_id"
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: "category_id"
+});
 
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: "product_tag",
+  foreignKey: "product_id"
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: "product_tag",
+  foreignKey: "tag_id"
+});
 
 module.exports = {
   Product,
