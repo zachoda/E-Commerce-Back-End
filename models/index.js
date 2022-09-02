79f3bc16ddb8d33a3d391f10feaaa464d269915d
Zach Oda <zjodachow@gmail.com>
2022-09-02 13:08:27 -0400

    fixed file structure

--- models/index.js
+++ models/index.js
@@ -18,13 +18,11 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: "product_tag",
   foreignKey: "product_id"
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: "product_tag",
   foreignKey: "tag_id"
 });
 
